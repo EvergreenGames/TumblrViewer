a2502e235921da326329427dcc0b5c76cf79e17c
Ruben Green <ragreen@fb.com>
2020-06-25 09:42:57 -0700

    Photo loading done MVP done

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Ruben Green on 6/25/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/PhotoCell.m
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.m
+//  Tumblr
+//
+//  Created by Ruben Green on 6/25/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import "PhotoCell.h"
+
+@implementation PhotoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
